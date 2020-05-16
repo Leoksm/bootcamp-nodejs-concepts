39b9ffd0744e2b768ee1c6ce37a3fc4d5e79ada5
Leo Moura <leo.moura@futurevaultinc.com>
2020-05-16 11:37:53 -0400

    Fixing small issues

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.put("/repositories/:id", (request, response) => {
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
   if (repositoryIndex < 0) {
-    return response.status(404).json({error: 'Repository not found'});
+    return response.status(400).json({error: 'Repository not found'});
   }
   
   const { likes } = repositories[repositoryIndex];
@@ -57,7 +57,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
   if (repositoryIndex < 0) {
-    return response.status(404).json({error: 'Repository not found'});
+    return response.status(400).json({error: 'Repository not found'});
   }
 
   repositories.splice(repositoryIndex, 1);
@@ -68,6 +68,10 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
   const repository = repositories.find(repository => repository.id === id);
+  if (!repository) {
+    return response.status(400).json({error: 'Repository not found'});
+  }
+
   repository.likes += 1;
   return response.json(repository);
 });
